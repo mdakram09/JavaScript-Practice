e35b99f999918de50e7128c040bc0773d24cae56
mdakram09 <iamakramkhan09@gmail.com>
2021-08-17 15:25:27 +0530

    practiced javascript

--- firstCode.js
+++ firstCode.js
@@ -1,14 +1,14 @@
-// console.log(Math.round(5.6))
-// console.log(Math.ceil(3.22))
-// console.log(2+2)
+console.log(Math.round(5.6))
+console.log(Math.ceil(3.22))
+console.log(2+2)
 
-// console.log("what is my name ?".length)
-// console.log("lol".length)
+console.log("what is my name ?".length)
+console.log("lol".length)
 
-// let firstName="Md Akram"
-// let lastName="Khan";
+let firstName="Md Akram"
+let lastName="Khan";
 
-// console.log("My full name is"+" "+firstName+" "+lastName);
+console.log("My full name is"+" "+firstName+" "+lastName);
 
 var msg="wish you a very happy new year"
 console.log(msg.length)
