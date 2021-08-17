6d35197907b374da7513196d64e12bb1bc7c816d
mdakram09 <iamakramkhan09@gmail.com>
2021-08-17 15:23:10 +0530

    practiced javascript

--- firstCode.js
+++ firstCode.js
@@ -1,3 +1,19 @@
-console.log(Math.round(5.6))
-console.log(Math.ceil(3.22))
-console.log(2+2)
+// console.log(Math.round(5.6))
+// console.log(Math.ceil(3.22))
+// console.log(2+2)
+
+// console.log("what is my name ?".length)
+// console.log("lol".length)
+
+// let firstName="Md Akram"
+// let lastName="Khan";
+
+// console.log("My full name is"+" "+firstName+" "+lastName);
+
+var msg="wish you a very happy new year"
+console.log(msg.length)
+console.log(msg[5])
+
+console.log(msg.toUpperCase())
+
+console.log(msg)
