f9633c93ecc2a54da6bde0758ee7700cf3c9f6f9
mdakram09 <iamakramkhan09@gmail.com>
2021-08-18 16:05:02 +0530

    practiced javascript strings

---
+++ strings.js
@@ -0,0 +1,12 @@
+let msg="My name is khan"
+console.log(msg[4])
+
+console.log("lol".length)
+console.log(msg.length)
+console.log("lol"+"lol")
+
+var fname="Md Akram";
+var lname="Khan";
+
+console.log("My name is "+fname+" "+lname);
+console.log(`My name is ${fname} ${lname}`)
