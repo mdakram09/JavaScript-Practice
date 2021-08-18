943008c6e6b0b0216e6867d957fd860e4ea83682
mdakram09 <iamakramkhan09@gmail.com>
2021-08-18 16:24:19 +0530

    practiced javascript strings

--- strings.js
+++ strings.js
@@ -18,3 +18,13 @@ console.log(msg.indexOf("khan"))
 console.log(msg.indexOf("a"))
 
 console.log(msg.slice(-4))
+
+console.log(msg.replace('khan','Md Akram Khan'))
+
+console.log("lol".repeat(10))
+
+console.log(Math.PI)
+
+console.log(Math.pow(2,3))
+
+console.log((Math.random())*10)
