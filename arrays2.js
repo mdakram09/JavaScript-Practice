d7643df64e14ae16594cf75ee18f63c44459e042
Md Akram Khan <md.khan@highradius.com>
2021-10-15 15:55:57 +0530

    practiced JavaScript arrays

---
+++ arrays2.js
@@ -0,0 +1,26 @@
+let marks=[76,82,84,85,86];
+const fruits=['Apple', 'Banana','Orange'];
+
+console.log(marks)
+console.log(fruits)
+
+marks.push(90)
+fruits.push('Pineapple')
+
+console.log(marks)
+console.log(fruits)
+
+marks.shift()
+marks.unshift(95)
+
+fruits.shift()
+fruits.unshift('Mango')
+
+console.log(marks)
+console.log(fruits)
+
+fruits.reverse()
+marks.reverse()
+
+console.log(marks)
+console.log(fruits)
