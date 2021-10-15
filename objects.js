e5f54a8055a52aa75c58ecee58c1dbe727fffbe0
Md Akram Khan <md.khan@highradius.com>
2021-10-15 16:54:45 +0530

    practiced JavaScript objects literals

---
+++ objects.js
@@ -0,0 +1,40 @@
+let Akram={
+    name: 'Md Akram Khan',
+    age: '21',
+    department: 'CSE',
+    skills:{
+        web: 'ReactJS',
+        ML:' Python',
+        NLP: 'RASA',
+    },
+    marks: [71,71,80,74,98]
+}
+
+console.log(Akram)
+console.log(Akram.age)
+console.log(Akram.skills)
+console.log(Akram.skills.web)
+console.log(Akram.marks)
+console.table(Akram)
+
+
+const myArr=[
+    {
+        breakFast: "Puri Sabji",
+        lunch: "Briyani",
+        Dinner: "Litti",
+    },
+    {
+        breakFast: "Puri Sabji",
+        lunch: "Briyani",
+        Dinner: "Litti",
+    },
+    {
+        breakFast: "Puri Sabji",
+        lunch: "Briyani",
+        Dinner: "Litti",
+    },
+]
+
+console.log(myArr)
+console.table(myArr)
